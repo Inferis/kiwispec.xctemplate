01fa71b1a23ad58d6bf1f219f24a3e5bccf8beac
Inferis <spam@inferis.org>
2013-11-14 21:13:55 +0100

    Added template.

---
+++ Kiwi Spec.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,40 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "Kiwi.h"
+#import "___VARIABLE_cutClass:identifier___.h"
+
+SPEC_BEGIN(___FILEBASENAMEASIDENTIFIER___)
+
+describe(@"<# Description for ___VARIABLE_cutClass:identifier___ spec #>", ^{
+    
+	beforeAll(^{ 
+		// Runs once at the start of the spec
+	});
+
+	afterAll(^{ 
+		// Runs once at the end of the spec
+	});
+
+	beforeEach(^{
+		// Runs before each example 
+    });
+   
+    afterEach(^{
+		// Runs after each example
+    });
+
+    it(@"<# example description #>", ^{
+
+    });
+
+});
+
+SPEC_END
+
+
